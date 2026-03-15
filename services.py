c75a86973f74bcdebc8fcae9eee6c9d66beaedf6
natchanonka68-commits <natchanon.ka.68@ubu.ac.th>
2026-03-15 20:39:13 +0700

    Update services.py

--- services.py
+++ services.py
@@ -1,1 +1,30 @@
+from models import Warrior, Mage
 
+class CharacterFactory:
+    @staticmethod
+    def create_character(char_type, name):
+        if char_type.lower() == "warrior":
+            return Warrior(name, 150, 25)
+        elif char_type.lower() == "mage":
+            return Mage(name, 80, 45)
+        return None
+
+class GameEngine:
+    def __init__(self, logger):
+        self.logger = logger
+        self.players = []
+
+    def add_player(self, character):
+        if character:
+            self.players.append(character)
+            self.logger.info(f"Character {character._name} added to the team.")
+
+    def run_battle_round(self):
+        results = []
+        for p in self.players:
+            results.append(p.unique_skill())
+        return results
+
+class ConsoleLogger:
+    def info(self, message):
+        print(f"[LOG] {message}")
